a4782df67933c83769fe4855b1cac9f9d9bcbd29
abilalkar <bilal.kar@ug.bilkent.edu.tr>
2022-12-16 17:07:57 +0300

    change build and settings accordingly

--- Instagram_clone/instagram_clone/lib/main.dart
+++ Instagram_clone/instagram_clone/lib/main.dart
@@ -1,12 +1,28 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/responsive/mobile_screen_layout.dart';
 import 'package:instagram_clone/responsive/responsive_layout_screen.dart';
 import 'package:instagram_clone/responsive/web_screen_layout.dart';
 import 'package:instagram_clone/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: FirebaseOptions(
+      apiKey: "AIzaSyBJ1yLzU0avB_SMW-Unlrebqd6RNdmLhTo",
+      appId: "1:430968434612:web:695fc019bab76640258ce1",
+      messagingSenderId: "430968434612",
+      projectId: "instagram-tut-fa05e",
+      storageBucket: "instagram-tut-fa05e.appspot.com",
+    ));
+  }
+  await Firebase.initializeApp(
+      //options: FirebaseOptions(apiKey: apiKey, appId: appId, messagingSenderId: messagingSenderId, projectId: projectId)
+      );
   runApp(const MyApp());
 }
 
