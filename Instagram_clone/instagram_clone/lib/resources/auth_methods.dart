ed186dffb455e2c4758e073a3205217c751f8d86
abilalkar <bilal.kar@ug.bilkent.edu.tr>
2022-12-17 18:50:17 +0300

    keep the signuop data in firestore

--- Instagram_clone/instagram_clone/lib/resources/auth_methods.dart
+++ Instagram_clone/instagram_clone/lib/resources/auth_methods.dart
@@ -1,9 +1,14 @@
+import 'dart:js_util';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/foundation.dart';
 
 class AuthMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   //sign the user up
   Future<String> signUpUser({
@@ -21,6 +26,18 @@ class AuthMethods {
           bio.isNotEmpty ||
           file != null) {
         //register
+        UserCredential credential = await _auth.createUserWithEmailAndPassword(
+            email: email, password: password);
+        //add user to database
+        await _firestore.collection('users').doc(credential.user!.uid).set({
+          'username': username,
+          'uid': credential.user!.uid,
+          'email': email,
+          'bio': bio,
+          'followers': [],
+          'following': [],
+        });
+        res = "success";
       }
     } catch (e) {
       res = e.toString();
